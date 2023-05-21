d17e07abcf7596ecdb5fdec92ea9b92cd1278092
Hadia Yehia <hadiayzmm@gmail.com>
2023-05-22 02:18:38 +0300

    home screen view

---
+++ iSport/HomeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  HomeCollectionViewCell.swift
+//  iSport
+//
+//  Created by Hadia Yehia on 22/05/2023.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var homeCellImg: UIImageView!
+    @IBOutlet weak var homeCellLabel: UILabel!
+}
